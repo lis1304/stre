0ebade17973553d38f5638b35e2e2c853f11754e
student <isl1304@yandex.ru>
2017-12-01 20:07:10 +0400

    les stream

--- App1.java
+++ App1.java
@@ -1,10 +1,13 @@
 package Stream;
 
+import Stream.seriaz.App;
+
 import java.io.*;
+import java.util.Scanner;
 
 public class App1 {
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+/*
          byte[] byteToWrite = {1,2,3};
          String fileName = "text.txt";
 
@@ -16,7 +19,6 @@ public class App1 {
 
         outFile.close();
 
-        /*----------------------------------------------------------------------*/
 
 
         FileInputStream inFile = new FileInputStream(fileName);
@@ -31,15 +33,65 @@ public class App1 {
         }
 
 
-        /*System.out.print(inFile.read());
         System.out.print(inFile.read());
-        System.out.print(inFile.read());*/
+        System.out.print(inFile.read());
+        System.out.print(inFile.read());
 
         inFile.close();
-
+       */
         /*----------------------------------------------------------------------*/
 
 
+/*
+        FileReader fr = new FileReader("text.txt");
+
+        BufferedReader br = new BufferedReader(fr);
+
+        String s;
+
+
+        while ((s = br.readLine()) != null){
+            System.out.println(s);
+        }
+*/
+        /*----------------------------------------------------------------------*/
+
+/*
+        FileReader fr = new FileReader("text.txt");
+
+        Scanner sc = new Scanner(fr);
+        sc.useDelimiter(", ");//кодировка
+        while (sc.hasNext()){
+            if (sc.hasNextInt()){
+                System.out.println(sc.nextInt() + ":int");
+            }
+            else
+            {
+                System.out.println(sc.next() + ":String");
+            }
+        }
+*/
+
+        App aa  = new App();
+        aa.a = 1.0;
+        aa.b = 5;
+
+        FileOutputStream fos = new FileOutputStream("seriaz.txt");
+
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+        oos.writeObject(aa);
+
+        oos.close();
+
+
+
+        FileInputStream fis = new FileInputStream("seriaz.txt");
+
+        ObjectInputStream ois = new ObjectInputStream(fis);
+
+        App bb = (App) ois.readObject();
+        System.out.println(bb.toString());
 
 
     }
---
+++ seriaz/App.java
@@ -0,0 +1,21 @@
+package Stream.seriaz;
+
+import java.io.*;
+
+public class App implements Serializable {
+
+         public double a;
+         public int b;
+
+    @Override
+    public String toString() {
+        return "App{" +
+                "a=" + a +
+                ", b=" + b +
+                '}';
+    }
+}
+
+
+
+
